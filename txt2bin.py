0fa0a10d8b7a762a5f29be1868e6c03769ebd7a6
John McMaster <johndmcmaster@gmail.com>
2020-07-13 23:06:41 -0700

    Major cleanup: remove static methods, clean up word vs byte, fix unix vs dos formatting, integrate pic1670
    Signed-off-by: John McMaster <johndmcmaster@gmail.com>

--- txt2bin.py
+++ txt2bin.py
@@ -5,7 +5,15 @@ import os
 from zorrom.util import add_bool_arg
 from zorrom.archs import arch2mr
 
-def run(arch, fn_in, fn_out, invert=None, rotate=None, flipx=False, flipy=False, verbose=False):
+
+def run(arch,
+        fn_in,
+        fn_out,
+        invert=None,
+        rotate=None,
+        flipx=False,
+        flipy=False,
+        verbose=False):
     try:
         mrc = arch2mr[arch]
     except KeyError:
@@ -15,8 +23,13 @@ def run(arch, fn_in, fn_out, invert=None, rotate=None, flipx=False, flipy=False,
     # There might be partial word conventions
 
     mr = mrc(verbose=verbose)
-    out_buff = mr.txt2bin(f_in, invert=invert, rotate=rotate, flipx=flipx, flipy=flipy)
+    out_buff = mr.txt2bin(f_in,
+                          invert=invert,
+                          rotate=rotate,
+                          flipx=flipx,
+                          flipy=flipy)
     open(fn_out, "wb").write(out_buff)
+
 
 def list_arch():
     for a in arch2mr.keys():
@@ -34,9 +47,16 @@ if __name__ == "__main__":
     parser.add_argument('--list-arch',
                         action='store_true',
                         help='Extended help')
-    parser.add_argument('--rotate', type=int, default=None, help='Rotate clockwise 90, 180, or 270 degrees')
-    parser.add_argument('--flipx', action="store_true", help='Mirror along x axis')
-    parser.add_argument('--flipy', action="store_true", help='Mirror along y axis')
+    parser.add_argument('--rotate',
+                        type=int,
+                        default=None,
+                        help='Rotate clockwise 90, 180, or 270 degrees')
+    parser.add_argument('--flipx',
+                        action="store_true",
+                        help='Mirror along x axis')
+    parser.add_argument('--flipy',
+                        action="store_true",
+                        help='Mirror along y axis')
     parser.add_argument('fn_in', nargs='?', help='.txt file in')
     parser.add_argument('fn_out', nargs='?', help='.bin file out')
     args = parser.parse_args()
--- zorrom/pic1670.py
+++ zorrom/pic1670.py
@@ -1,6 +1,5 @@
 from zorrom import mrom
 '''
-
 Reference
 https://siliconpr0n.org/map/apple/pic1670-adb-turbo/mz_mit20x/
 
@@ -8,43 +7,33 @@ https://siliconpr0n.org/map/apple/pic1670-adb-turbo/mz_mit20x/
 
 
 class PIC1670(mrom.MaskROM):
-    @staticmethod
     def desc(self):
         return 'PIC1670'
 
-    @staticmethod
-    def bigendian(self):
-        # pic16 is little endian
-        return 0
+    def endian(self):
+        return "little"
 
-    @staticmethod
-    def bitwidth(self):
-        # pic16 has 13 bit words
+    def word_bits(self):
         return 13
 
-    @staticmethod
-    def txtwh():
+    def txtwh(self):
         # 8 groups of 8 bits across, 13 groups of 16 bits down.
-        return (8*8, 13 * 16)
+        return (8 * 8, 13 * 16)
 
-    @staticmethod
-    def txtgroups():
-        # Use monkey convention breaking on groups of 8
-        # Actual has no col breaks and every second row break is larger
-        return range(8, 32 * 8, 8), range(8, 32 * 8, 8)
-
-    @staticmethod
-    def invert():
+    def invert(self):
         return True
 
-    '''Given binary (byte offset, bit index) return image row/col'''
+    '''Given binary (word offset, bit index) return image row/col'''
+
     def oi2cr(self, offset, maski):
         #logic from modemmap.cc
-        b = 12-maski
+        b = 12 - maski
         col = offset & 0x3f
         if (col & 8):
             col ^= 7
         col ^= 0x38
-        row = 0 ^ (8 if (offset & 0x200) else 0) | (4 if (offset & 0x100) else 0) | (2 if(offset & 0x080) else 0) | (1 if(offset & 0x040) else 0)
-        row += b*16
+        row = 0 ^ (8 if (offset & 0x200) else 0) | (4 if (
+            offset & 0x100) else 0) | (2 if (offset & 0x080) else
+                                       0) | (1 if (offset & 0x040) else 0)
+        row += b * 16
         return (col, row)
